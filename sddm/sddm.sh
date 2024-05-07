52fc95776f419ac2a9a6b5500a186eddc265fe2d
ab0z3in4 <social.media.abozeina@gmail.com>
2024-05-07 11:25:36 +0300

    Push dotFiles

---
+++ sddm/sddm.sh
@@ -0,0 +1,21 @@
+echo "Install sddm"
+yay -S --noconfirm sddm sddm-sugar-candy-git --ask 4
+if [ -f /etc/systemd/system/display-manager.service ]; then
+    sudo rm /etc/systemd/system/display-manager.service
+fi
+sudo systemctl enable sddm.service
+if [ ! -d /etc/sddm.conf.d/ ]; then
+    sudo mkdir /etc/sddm.conf.d
+    echo "Folder /etc/sddm.conf.d created."
+fi
+sudo cp sddm.conf /etc/sddm.conf.d/
+echo "File /etc/sddm.conf.d/sddm.conf updated."
+
+if [ -f /usr/share/sddm/themes/sugar-candy/theme.conf ]; then
+    # Cache file for holding the current wallpaper
+    sudo cp default.jpg /usr/share/sddm/themes/sugar-candy/Backgrounds/current_wallpaper.jpg
+    echo "Default wallpaper copied into /usr/share/sddm/themes/sugar-candy/Backgrounds/"
+    sudo cp theme.conf /usr/share/sddm/themes/sugar-candy/
+    sudo sed -i 's/CURRENTWALLPAPER/'"current_wallpaper.jpg"'/' /usr/share/sddm/themes/sugar-candy/theme.conf
+    echo "File theme.conf updated in /usr/share/sddm/themes/sugar-candy/"
+fi
