cced1f940e1d0a47498c0a5896e7ca188291e912
ab0z3in4 <social.media.abozeina@gmail.com>
2024-11-24 09:40:47 +0200

    Add Neovim Config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "lua", "bash", "python", "javascript", "html", "css" },
+      sync_install = false,
+      highlight = { enable = true },
+      autopairs = { enable = true },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false }
+    })
+  end
+}
