3374d4a560b5a160de16991ee3f19765e50e661e
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2024-12-11 09:55:59 +0200

    Update lualine.lua

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,6 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup({
-      options = {
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        ignore_focus = { "NvimTree" },
-      },
-      sections = {
-        lualine_a = {},
-        lualine_b = { "branch" },
-        lualine_c = { "diagnostics" },
-        lualine_y = { "progress" },
-        lualine_z = {},
-      },
-      extensions = { "quickfix", "man", "fugitive" },
-    })
+    require("lualine").setup()
   end,
 }
