8a8b3805baeebf6ec568495a0c6e7e2d97716868
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2024-12-13 10:53:56 +0200

    Update none-ls.lua

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -11,7 +11,6 @@ return {
         formatting.prettier,
         formatting.black,
         null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.diagnostics.solhint,
         null_ls.builtins.completion.spell,
       },
     })
