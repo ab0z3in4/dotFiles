137ed57eef3e0b7f2d910ad2ea4c09bf15bf67f7
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-01-28 15:59:29 +0200

    Update brightness-control.sh

--- .config/bspwm/scripts/brightness-control.sh
+++ .config/bspwm/scripts/brightness-control.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# You can call this script like this:
-# $ ./brightness-control.sh up
-# $ ./brightness-control.sh down
-
-# Script inspired by these wonderful people:
-# https://github.com/dastorm/volume-notification-dunst/blob/master/volume.sh
-# https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
-
-# Icon path: /usr/share/icons/Papirus/16x16/apps
 icon="display-brightness"
 
 function get_brightness {
-  xbacklight -get | cut -d '.' -f 1
+  brightnessctl get | cut -d '.' -f 1
 }
 
 function send_notification {
@@ -21,11 +12,11 @@ function send_notification {
 
 case $1 in
   up)
-    xbacklight -inc 5
+    brightnessctl set 5%+
     send_notification "$(get_brightness)"
     ;;
   down)
-    xbacklight -dec 5
+    brightnessctl set 5%-
     send_notification "$(get_brightness)"
     ;;
 esac
