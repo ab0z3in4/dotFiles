2d34e54a8917de3e674a6f15023f3931270cdfe2
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-01-29 07:55:45 +0200

    Update configuration.nix

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -102,12 +102,18 @@ in
         model = "pc105";
         options = "grp:win_space_toggle";
       };
-    };
-    displayManager = {
-      defaultSession = "none+bspwm";
-      autoLogin = {
-        enable = true;
-        user = "ab0z3in4";
+      displayManager = {
+        lightdm = {
+          enable = true;
+          greeters.slick = {
+            enable = true;
+            theme.name = "adw-gtk3-dark";
+            iconTheme.name = "Papirus-Dark";
+            cursorTheme.name = "Bibata-Modern-Classic";
+            cursorTheme.size = 16;
+            font.name = "JetBrains Mono";
+          };
+        };
       };
     };
   };
