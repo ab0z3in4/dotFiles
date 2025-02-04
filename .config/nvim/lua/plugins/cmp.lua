20a33890c27b11293882c7b1be9bb7c528399156
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-02-04 07:18:16 +0200

    Update cmp.lua

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -18,46 +18,6 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
-  --[[mapping = cmp.mapping.preset.insert {
-    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item { behavior = types.cmp.SelectBehavior.Select }, { "i", "c" }),
-    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item { behavior = types.cmp.SelectBehavior.Select }, { "i", "c" }),
-    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item { behavior = types.cmp.SelectBehavior.Select }, { "i", "c" }),
-    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item { behavior = types.cmp.SelectBehavior.Select }, { "i", "c" }),
-    ["<C-h>"] = function()
-      if cmp.visible_docs() then
-        cmp.close_docs()
-      else
-        cmp.open_docs()
-      end
-    end,
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        require("neotab").tabout()
-      else
-        require("neotab").tabout()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },]]
   mapping = cmp.mapping.preset.insert {
     ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
     ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
